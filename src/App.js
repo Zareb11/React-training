617cf9d2d9979bfed9ad28778ebd61b40fb35918
Jacob Witbooi <jacobrwitbooi@gmail.com>
2022-09-08 09:47:20 +0200

    New project with new features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import Todo from "./components/Todo";
+import { Routes, Route } from 'react-router-dom';
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
-  return (<div>My Todos
-    <Todo text='Learn React'/>
-    <Todo text='Master React'/>
-    <Todo text='Explore full React course'/>
-  </div>);
-
+  return (
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />}/>
+        <Route path='/new-meetup' element={<NewMeetupPage />}/>
+        <Route path='/favorites' element={<FavoritesPage />}/>
+      
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
