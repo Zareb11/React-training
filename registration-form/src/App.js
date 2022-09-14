12c6d389ddb252c1fc0b5d0cb3603feb48012abe
Jacob Witbooi <jacobrwitbooi@gmail.com>
2022-09-14 13:30:32 +0200

    Added Register form

---
+++ registration-form/src/App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form';
+import {Route, Routes} from 'react-router-dom';
+import './App.css';
+
+
+function App() {
+  return (
+  <div className='App'>
+    <Form></Form>
+  </div>
+  
+  );
+}
+
+export default App;
---
+++ registration-form/src/components/Form.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import bgImg from '../assests/img1.jpg'
+
+
+export default function Form() {
+  return (
+    <section>
+    <div className="register">
+        <div className="col-1">
+            <h2>Sign In</h2>
+            <span>register and enjoy the service</span>
+
+            <form id='form' className='flex flex-col' >
+                <input type="text"  placeholder='username' />
+                <input type="text"  placeholder='password' />
+                <input type="text"  placeholder='confirm password' />
+                <input type="text"  placeholder='mobile number' />
+                <button className='btn'>Sign In</button>
+            </form>
+
+        </div>
+        <div className="col-2">
+            <img src={bgImg} alt="" />
+        </div>
+    </div>
+</section>
+  )
+}
